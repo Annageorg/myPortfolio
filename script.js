9548ebb96e4aa827a168cdc6aa1cbc69a2933800
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-06-07 14:55:37 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,97 @@
+//Mouse circle
+const mouseCircle = document.querySelector(".mouse-circle");
+const mouseDot = document.querySelector(".mouse-dot");
+
+const mouseCircleFn = (x, y) => {
+    mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
+    mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
+};
+//End of Mouse circle
+
+// Animated Circles
+const circles = document.querySelectorAll('.circle')
+const mainImg = document.querySelector('.main-circle img')
+
+let mX = 0;
+let mY = 0;
+const z = 100;
+const animateCircles = (e, x, y) => {
+    if (x < mX) {
+        circles.forEach((circle) => {
+            circle.style.left = `${z}px`
+        })
+        mainImg.style.left = `${z}px`
+    } else if (x > mX) {
+        circles.forEach((circle) => {
+            circle.style.left = `-${z}px`
+        })
+        mainImg.style.left = `-${z}px`
+    }
+    if (y < mY) {
+        circles.forEach((circle) => {
+            circle.style.top = `${z}px`
+        })
+        mainImg.style.top = `${z}px`
+    } else if (y > mY) {
+        circles.forEach((circle) => {
+            circle.style.top = `-${z}px`
+        })
+        mainImg.style.top = `-${z}px`
+    }
+    mX = e.clientX;
+    mY = e.clientY;
+}
+// End of Animated Circles
+
+document.body.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+  
+    mouseCircleFn(x, y);
+    animateCircles(e, x, y);
+});
+
+document.body.addEventListener('mouseleave' , (e) => {
+    mouseCircle.style.opacity = '0';
+    mouseDot.style.opacity = '0';
+})
+
+// Main button
+const mainBtns = document.querySelectorAll('.main-btn')
+
+
+mainBtns.forEach(btn => {
+    let ripple;
+    btn.addEventListener('mouseenter', (e) => {
+        const left = e.clientX - e.target.getBoundingClientRect().left;
+        const top = e.clientY - e.target.getBoundingClientRect().top;
+    
+        ripple = document.createElement('div')
+        ripple.classList.add('ripple')
+        ripple.style.left=`${left}px`    
+        ripple.style.top=`${top}px`
+        btn.prepend(ripple)    
+    })
+    btn.addEventListener('mouseleave', () => {
+        btn.removeChild(ripple)
+    })
+})
+
+
+// End of Main button
+
+// About me text
+const aboutMeText = document.querySelector('.about-me-text');
+const aboutMeTextContent = `I am an MEng, MSc, MPhil mechanical engineer. As my next step, I am pursuing a career change to Software Development and I am looking for roles where I can put my strengths to use to help people.`;
+
+Array.from(aboutMeTextContent).forEach(char => {
+    const span = document.createElement('span');
+    span.textContent = char;
+    aboutMeText.appendChild(span);
+
+    span.addEventListener('mouseenter', (e) => {
+        e.target.style.animation = 'aboutMeTextAnim 15s infinite'
+    })
+})
+// End of About me text
+
