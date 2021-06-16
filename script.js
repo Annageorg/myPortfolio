d81f0374fab4226a27b06513f9be19d1e6132d35
Anna Georgiadou <a.d.georgiadou@gmail.com>
2021-06-16 21:31:39 +0100

    progress navigation and mouse effects

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
+let mouseCircleBool = true;
+
 const mouseCircleFn = (x, y) => {
-    mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
+    mouseCircleBool &&
+        (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`)
+    
     mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
 //End of Mouse circle
@@ -43,12 +47,70 @@ const animateCircles = (e, x, y) => {
 }
 // End of Animated Circles
 
+let hoveredElPosition = []
+
+const stickyElement = (x, y, hoveredEl) => {
+    // Sticky Element
+    if (hoveredEl.classList.contains("sticky")) {
+        hoveredElPosition.length < 1 &&
+            (hoveredElPosition = [hoveredEl.offsetTop, hoveredEl.offsetLeft])
+
+        hoveredEl.style.cssText = `top: ${y}px; left: ${x}px;`
+
+        if (hoveredEl.offsetTop <= hoveredElPosition[0] - 100 ||
+            hoveredEl.offsetTop >= hoveredElPosition[0] + 100 ||
+            hoveredEl.offsetLeft <= hoveredElPosition[1] - 100 ||
+            hoveredEl.offsetLeft >= hoveredElPosition[1] + 100
+        ) {
+            hoveredEl.style.cssText = ""
+            hoveredElPosition = []
+        }
+        hoveredEl.onmouseleave = () => {
+            hoveredEl.style.cssText = ""
+            hoveredElPosition = []
+        }
+    }
+    // End of Sticky Element
+}
+
+// Mouse circle transform
+const mouseCircleTransform = (hoveredEl) => {
+    if (hoveredEl.classList.contains('pointer-enter')) {
+        hoveredEl.onmousemove = () => {
+            mouseCircleBool = false
+            mouseCircle.style.cssText = `
+                width: ${hoveredEl.getBoundingClientRect().width}px;
+                height: ${hoveredEl.getBoundingClientRect().height}px;
+                top: ${hoveredEl.getBoundingClientRect().top}px;
+                left: ${hoveredEl.getBoundingClientRect().left}px;
+                opacity: 1;
+                transform: translate(0, 0);
+                animation: none;
+                border-radius: ${getComputedStyle(hoveredEl).borderBottomLeftRadius};
+                transition: width 0.5s, height 0.5s, top 0.5s, left 0.5s, transform 0.5s, border-radius 0.5s`
+        }
+        hoveredEl.onmouseleave = () => {
+            mouseCircleBool = true
+        }
+        document.onscroll = () => {
+            if (!mouseCircleBool) {
+                mouseCircle.style.top = `${hoveredEl.getBoundingClientRect().top}px`
+            }
+        }
+    }
+}
+// End of Mouse circle transform
+
 document.body.addEventListener("mousemove", (e) => {
     let x = e.clientX;
     let y = e.clientY;
 
     mouseCircleFn(x, y);
     animateCircles(e, x, y);
+
+    const hoveredEl = document.elementFromPoint(x, y)
+    stickyElement(x, y, hoveredEl)
+    mouseCircleTransform(hoveredEl)
 });
 
 document.body.addEventListener('mouseleave', (e) => {
@@ -78,18 +140,74 @@ mainBtns.forEach(btn => {
 })
 // End of Main button
 
+// Progress bar
+const sections = document.querySelectorAll('section')
+const progressBar = document.querySelector('.progress-bar')
+const halfCircles = document.querySelectorAll('.half-circle')
+const halfCircleTop = document.querySelector('.half-circle-top')
+const progressBarCircle = document.querySelector('.progress-bar-circle')
+
+const progressBarFn = () => {
+    const pageViewportHeight = window.innerHeight
+    const pageHeight = document.documentElement.scrollHeight
+    const scrolledPortion = window.pageYOffset
+
+    const scrolledPortionDegree = (scrolledPortion / (pageHeight - pageViewportHeight)) * 360
+
+    halfCircles.forEach((el) => {
+        el.style.transform = `rotate(${scrolledPortionDegree}deg)`
+
+        if (scrolledPortionDegree >= 180) {
+            halfCircles[0].style.transform = 'rotate(180deg)'
+            halfCircleTop.style.opacity = "0"
+        } else {
+            halfCircleTop.style.opacity = "1"
+        }
+    })
+
+    const scrollBool = scrolledPortion + pageViewportHeight === pageHeight
+    // Progress bar click
+    progressBar.onclick = (e) => {
+        e.preventDefault()
+
+        const sectionPositions = Array.from(sections).map((section) => {
+            return scrolledPortion + section.getBoundingClientRect().top
+        })
+
+        const position = sectionPositions.find((sectionPosition) => {
+            return sectionPosition > scrolledPortion
+        })
+
+        scrollBool ? window.scrollTo(0, 0) : window.scrollTo(0, position)
+    }
+    // End of Progress bar click
+
+    // Rotate  arrow
+    if (scrollBool) {
+        progressBarCircle.style.transform = "rotate(180deg)"
+    } else {
+        progressBarCircle.style.transform = "rotate(0deg)"
+    }
+    // End of Rotate  arrow
+}
+progressBarFn();
+// End of Progress bar
+
 // Navigation
 const menuIcon = document.querySelector('.menu-icon')
 const navbar = document.querySelector('.navbar')
 
-document.addEventListener('scroll', () => {
+const scrollFn = () => {
     menuIcon.classList.add('show-menu-icon')
     navbar.classList.add('hide-navbar')
-    if (window.scrollY === 0 ) {
+    if (window.scrollY === 0) {
         menuIcon.classList.remove('show-menu-icon')
         navbar.classList.remove('hide-navbar')
     }
-})
+    progressBarFn()
+}
+
+document.addEventListener('scroll', scrollFn)
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.remove('show-menu-icon')
     navbar.classList.remove('hide-navbar')
@@ -157,19 +275,19 @@ const hideProjects = (project, i) => {
 
 projectsBtn.addEventListener("click", (e) => {
     e.preventDefault();
-  
+
     projectsBtn.firstElementChild.nextElementSibling.classList.toggle("change");
-  
+
     showHideBool
-      ? (projectsBtnText.textContent = "Show Less")
-      : (projectsBtnText.textContent = "Show More");
-  
+        ? (projectsBtnText.textContent = "Show Less")
+        : (projectsBtnText.textContent = "Show More");
+
     projects.forEach((project, i) => {
-      i >= 6 &&
-        (showHideBool ? showProjects(project, i) : hideProjects(project, i));
+        i >= 6 &&
+            (showHideBool ? showProjects(project, i) : hideProjects(project, i));
     });
     showHideBool = !showHideBool;
-  });
+});
 // End of Projects button
 
 // End of Projects
@@ -185,37 +303,37 @@ formInputs.forEach((input) => {
         setTimeout(() => {
             formHeading.textContent = `Your ${input.placeholder}`
             formHeading.style.opacity = '1'
-        }, 300)        
+        }, 300)
     })
     input.addEventListener('blur', () => {
         formHeading.style.opacity = '0'
         setTimeout(() => {
             formHeading.textContent = `Let's Talk`
             formHeading.style.opacity = '1'
-        }, 300)        
+        }, 300)
     })
 })
 
 // **************************Form submission
 let form = document.getElementById("my-form");
-   
+
 async function handleSubmit(event) {
-  event.preventDefault();
-  let status = document.getElementById("my-form-status");
-  let data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-  }).then(response => {
-    status.innerHTML = "Thank you for your submission!";
-    status.classList.add('my-form-status')
-    form.reset()
-  }).catch(error => {
-    status.innerHTML = "Oops! Something went wrong"
-  });
+    event.preventDefault();
+    let status = document.getElementById("my-form-status");
+    let data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        status.innerHTML = "Thank you for your submission!";
+        status.classList.add('my-form-status')
+        form.reset()
+    }).catch(error => {
+        status.innerHTML = "Oops! Something went wrong"
+    });
 }
 form.addEventListener("submit", handleSubmit)
 // End of Form 
